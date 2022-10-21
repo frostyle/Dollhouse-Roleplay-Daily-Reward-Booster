fdc9d9baf7c91caa74003a178fa21ec1450e035d
robson <75824844+frosty171@users.noreply.github.com>
2022-10-21 14:05:22 -0300

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,7 @@
+for i = 1, 1000 dohttps://github.com/frosty171/Dollhouse-Roleplay-Daily-Reward-Booster-
+   for i = 1, 100 do
+       for i = 1, 10 do
+           game.ReplicatedStorage.ClaimReward:FireServer()
+       end
+   end
+end
